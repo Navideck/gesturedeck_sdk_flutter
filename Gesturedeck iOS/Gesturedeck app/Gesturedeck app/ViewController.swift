e06eb4e3a9632aa553deec9d2cc206565fd89745
Foti Dim <fdimanidis@gmail.com>
2022-10-02 20:35:59 +0200

    Change colors using button in example iOS app

--- Gesturedeck iOS/Gesturedeck app/Gesturedeck app/ViewController.swift
+++ Gesturedeck iOS/Gesturedeck app/Gesturedeck app/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func buttonPress(_ sender: Any) {
+        if view.backgroundColor == UIColor.white {
+            view.backgroundColor = UIColor.black
+        } else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
