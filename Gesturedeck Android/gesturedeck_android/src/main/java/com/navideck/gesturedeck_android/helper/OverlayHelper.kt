7d04f69dd1122a8d314f0974b32c29a5a78b8c0e
Rohit Sangwan <rohitsangwan647@gmail.com>
2022-12-05 21:48:55 +0530

    Fix volume bar postition in landscape

--- Gesturedeck Android/gesturedeck_android/src/main/java/com/navideck/gesturedeck_android/helper/OverlayHelper.kt
+++ Gesturedeck Android/gesturedeck_android/src/main/java/com/navideck/gesturedeck_android/helper/OverlayHelper.kt
@@ -283,44 +283,28 @@ class OverlayHelper(
     }
 
     private fun initVolumeBar() {
-        val screenWidth = ScreenInfo.getScreenSize(context).width
-        val screenOrientationMode = ScreenInfo.getOrientationMode(context)
-
-        val barWidth = 150
-        volumeBar.width = barWidth
-
-//        if (screenOrientationMode == OrientationMode.LANDSCAPE) {
-//            volumeBar.setBarX(screenWidth - barWidth)
-//        } else {
-//            volumeBar.setBarX(0)
-//        }
-
-        when (screenOrientationMode) {
-            OrientationMode.LANDSCAPE -> {
-                val uncoveredSpace = ScreenInfo.getLandscapeModeUncoveredSpace(context)
-                var xAxis = screenWidth
-                if (uncoveredSpace > 0) {
-                    val halfWidth = barWidth / 2
-                    val requiredSpace = uncoveredSpace + (halfWidth / 2)
-                    xAxis -= requiredSpace
-                    volumeBar.width = halfWidth
+        ScreenInfo.getDisplayInsets(getRootView()) {
+            var leftGap = 0
+            if (it != null) leftGap = it.left
+            var shouldShiftLeft = false
+            when (ScreenInfo.getOrientationMode(context)) {
+                OrientationMode.LANDSCAPE -> {
+                    val isCutoutCovered: Boolean = ScreenInfo.isCutoutModeCovered(context) ?: true
+                    shouldShiftLeft = leftGap > 0 && isCutoutCovered
                 }
-                volumeBar.setBarX(xAxis)
-            }
-            OrientationMode.REVERSE_LANDSCAPE -> {
-                val uncoveredSpace = ScreenInfo.getLandscapeModeUncoveredSpace(context)
-                var xAxis = 0
-                if (uncoveredSpace > 0) {
-                    val halfWidth = barWidth / 2
-                    val requiredSpace = uncoveredSpace + (halfWidth / 2)
-                    xAxis += requiredSpace
-                    volumeBar.width = halfWidth
+                OrientationMode.REVERSE_LANDSCAPE -> {
+                    shouldShiftLeft = leftGap > 0
                 }
-                volumeBar.setBarX(xAxis)
+                else -> {}
             }
-            else -> {
-                volumeBar.setBarX(0)
+            val barWidth = 150
+            var xAxis = 0
+            volumeBar.width = barWidth
+            if (shouldShiftLeft) {
+                xAxis += leftGap + (barWidth / 4)
+                volumeBar.width = barWidth / 2
             }
+            volumeBar.setBarX(xAxis)
         }
     }
 
@@ -657,8 +641,8 @@ class OverlayHelper(
 
     // Helper Methods
     private fun measureAndLayout(context: Context, toMeasure: View) {
-        var dpHeight: Int = ScreenInfo.getScreenSize(context).height
-        var dpWidth: Int = ScreenInfo.getScreenSize(context).width
+        val dpHeight: Int = ScreenInfo.getScreenSize(context).height
+        val dpWidth: Int = ScreenInfo.getScreenSize(context).width
         toMeasure.measure(
             View.MeasureSpec.makeMeasureSpec(dpWidth, View.MeasureSpec.EXACTLY),
             View.MeasureSpec.makeMeasureSpec(dpHeight, View.MeasureSpec.EXACTLY)
