b51439017d3353d5976b60047fed5de722397580
Foti Dim <fdimanidis@gmail.com>
2023-01-03 18:47:56 +0100

    Refactor Android

--- android/src/main/kotlin/com/navideck/gesturedeck_flutter/GesturedeckFlutterPlugin.kt
+++ android/src/main/kotlin/com/navideck/gesturedeck_flutter/GesturedeckFlutterPlugin.kt
@@ -2,34 +2,112 @@ package com.navideck.gesturedeck_flutter
 
 import androidx.annotation.NonNull
 
+import android.app.Activity
+import android.os.Build
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.WindowManager
+import com.navideck.gesturedeck_sdk.Gesturedeck
+import com.navideck.gesturedeck_sdk.model.GesturedeckEvent
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
+import io.flutter.embedding.engine.renderer.FlutterRenderer
+import io.flutter.plugin.common.*
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** GesturedeckFlutterPlugin */
 class GesturedeckFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "gesturedeck_flutter")
-    channel.setMethodCallHandler(this)
+  companion object {
+      var instance: GesturedeckFlutterPlugin? = null
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
+  private var touchEventSink: EventChannel.EventSink? = null
+  private lateinit var channel: MethodChannel
+  private lateinit var touchEventResult: EventChannel
+  private lateinit var renderer: FlutterRenderer
+  private var gesturedeck: Gesturedeck? = null
+
+  private fun initGesturedeck(activity: Activity) {
+      gesturedeck = Gesturedeck(
+          context = activity,
+          bitmapCallback = { renderer.bitmap },
+          gestureCallbacks = { event ->
+              when (event) {
+                  GesturedeckEvent.SWIPE_RIGHT -> {
+                      touchEventSink?.success("swipedRight")
+                  }
+                  GesturedeckEvent.SWIPE_LEFT -> {
+                      touchEventSink?.success("swipedLeft")
+                  }
+                  GesturedeckEvent.TAP -> {
+                      touchEventSink?.success("tap")
+                  }
+                  else -> {}
+              }
+          }
+      )
+  }
+
+  fun dispatchTouchEvent(event: MotionEvent, activity: Activity) {
+      if (touchEventSink == null) return
+      if (gesturedeck == null) initGesturedeck(activity)
+      gesturedeck?.onTouchEvents(event)
+  }
+
+  fun dispatchKeyEvent(event: KeyEvent): Boolean {
+      return gesturedeck?.onKeyEvents(event) ?: false
+  }
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+      channel = MethodChannel(
+          flutterPluginBinding.binaryMessenger,
+          "com.navideck.gesturedeck.method"
+      )
+      channel.setMethodCallHandler(this)
+      touchEventResult =
+          EventChannel(flutterPluginBinding.binaryMessenger, "com.navideck.gesturedeck")
+      touchEventResult.setStreamHandler(this)
+      instance = this
+      renderer = flutterPluginBinding.flutterEngine.renderer
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
       result.notImplemented()
-    }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+      channel.setMethodCallHandler(null)
+      touchEventResult.setStreamHandler(null)
+      instance = null
+  }
+
+
+  /// Call this method in onCreate of MainActivity, to extend the app's UI around the notch
+  fun extendAroundNotch(activity: Activity) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+          activity.window.attributes.layoutInDisplayCutoutMode =
+              WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+      }
+  }
+
+
+  override fun onListen(args: Any?, eventSink: EventChannel.EventSink?) {
+      val map = args as? Map<*, *> ?: return
+      when (map["name"]) {
+          "touchEvent" -> {
+              touchEventSink = eventSink
+          }
+      }
+  }
+
+  override fun onCancel(args: Any?) {
+      val map = args as? Map<*, *> ?: return
+      when (map["name"]) {
+          "touchEvent" -> {
+              touchEventSink = null
+              gesturedeck?.dispose()
+              gesturedeck = null
+          }
+      }
   }
 }
--- example/android/app/src/main/kotlin/com/navideck/gesturedeck_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/navideck/gesturedeck_flutter_example/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.navideck.gesturedeck_flutter_example
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.view.*
+import com.navideck.gesturedeck.GesturedeckPlugin
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GesturedeckPlugin.instance?.extendAroundNotch(this)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        GesturedeckPlugin.instance?.dispatchTouchEvent(event, activity)
+        return super.dispatchTouchEvent(event)
+    }
+
+    // add this to Take Control over VolumeKeys , and show our custom UI instead of Device VolumeDialog
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        return GesturedeckPlugin.instance?.dispatchKeyEvent(event) ?: false
+    }
 }
